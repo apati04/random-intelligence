8746fa24a423468615f5b4d8a62a84f2e05812cd
Andrew Patipaksiri <apax714@gmail.com>
2017-07-22 22:26:26 -0700

    [app] add lifecycle method to prep for api call

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
 import Main from './main';
+import axios from 'axios';
 
 
 
 export default class App extends Component {
+ constructor(props) {
+   super(props);
+   this.state = {};
+ }
+
+ componentWillMount() {
+   console.log('component ready');
+ }
   render() {
     return (
       <MuiThemeProvider>
---
+++ src/components/currentCount.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function CurrentCount ({counter,  total}) {
+  return (
+    <div className="currCount">
+      Question Number <span>{counter}</span> of <span>{total}</span>
+    </div>
+  )
+}
