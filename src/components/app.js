e930bb0bf5383929a3b0d21a2a502f2b5f55df19
Andrew Patipaksiri <apax714@gmail.com>
2017-07-22 20:59:52 -0700

    [style] add materal-ui

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import FlatButton from 'material-ui/FlatButton';
+import {deepOrange500} from 'material-ui/styles/colors';
+import AppBar from 'material-ui/AppBar';
+
+const muiTheme = getMuiTheme({
+  palette: {
+    accent1Color: deepOrange500,
+  },
+});
+
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        Home Page
-      </div>
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <div>
+          App
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/app';
 import reducers from './reducers';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
